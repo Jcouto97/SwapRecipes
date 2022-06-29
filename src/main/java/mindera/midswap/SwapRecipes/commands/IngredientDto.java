82a6606e11baa3727f082bddbba24675c82fd89a
jon <joao.couto@mindswap.academy>
2022-06-29 18:48:50 +0100

    Ingredient branch

--- src/main/java/mindera/midswap/SwapRecipes/commands/IngredientDto.java
+++ src/main/java/mindera/midswap/SwapRecipes/commands/IngredientDto.java
@@ -1,4 +1,21 @@
 package mindera.midswap.SwapRecipes.commands;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.*;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
+
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
 public class IngredientDto {
+    @JsonProperty(access = JsonProperty.Access.READ_ONLY)
+    private Long id;
+
+    @NotEmpty
+    @Size(max = 20)
+    private String name;
 }
