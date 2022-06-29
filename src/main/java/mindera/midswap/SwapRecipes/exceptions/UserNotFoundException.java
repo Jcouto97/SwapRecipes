5465d0cb93b73b55c4b12d57aed81ffcd50c7f24
alicjakropa <alakrop@gmail.com>
2022-06-29 18:51:35 +0200

    1st commit

---
+++ src/main/java/mindera/midswap/SwapRecipes/exceptions/UserNotFoundException.java
@@ -0,0 +1,7 @@
+package mindera.midswap.SwapRecipes.exceptions;
+
+public class UserNotFoundException extends RuntimeException {
+    public UserNotFoundException(){
+        super("User not found");
+    }
+}
