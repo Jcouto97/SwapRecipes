9a6f0d339d39c58c850467b8e7ad9d19613ab025
alicjakropa <alakrop@gmail.com>
2022-06-29 19:12:57 +0200

    recipe model

--- src/main/java/mindera/midswap/SwapRecipes/persistence/models/Recipe.java
+++ src/main/java/mindera/midswap/SwapRecipes/persistence/models/Recipe.java
@@ -1,9 +1,11 @@
 package mindera.midswap.SwapRecipes.persistence.models;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.*;
 import org.hibernate.annotations.Generated;
 
 import javax.persistence.*;
+import java.util.List;
 
 
 @Setter
@@ -20,9 +22,18 @@ public class Recipe {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(nullable = false, unique = true, updatable = false)
-    Long id;
+    private Long id;
 
+    @JsonIgnore
+    @ManyToMany(cascade = CascadeType.DETACH)
+    @JoinColumn(name = "userId", referencedColumnName = "id")
+    private User user;
 
+    @JsonIgnore
+    @ManyToMany(cascade = CascadeType.DETACH)
+    @JoinColumn(name = "ingredientId", referencedColumnName = "id")
+    private List<Ingredient> ingredientList;
 
+    private String description;
 
 }
