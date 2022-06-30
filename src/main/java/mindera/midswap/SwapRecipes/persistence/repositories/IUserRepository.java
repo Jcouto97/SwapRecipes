727b5089a27248b2a59e1537bcf7f934d4cba7e8
alicjakropa <alakrop@gmail.com>
2022-06-30 14:49:19 +0200

    update do many to many

--- src/main/java/mindera/midswap/SwapRecipes/persistence/repositories/IUserRepository.java
+++ src/main/java/mindera/midswap/SwapRecipes/persistence/repositories/IUserRepository.java
@@ -2,13 +2,16 @@ package mindera.midswap.SwapRecipes.persistence.repositories;
 
 
 import mindera.midswap.SwapRecipes.commands.UserDto;
+import mindera.midswap.SwapRecipes.persistence.models.Ingredient;
 import mindera.midswap.SwapRecipes.persistence.models.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface IUserRepository extends JpaRepository<User, Long> {
 
-
+    public Optional<User> findById(Long id);
 
 }
--- src/main/java/mindera/midswap/SwapRecipes/services/RecipeService.java
+++ src/main/java/mindera/midswap/SwapRecipes/services/RecipeService.java
@@ -1,17 +1,19 @@
 package mindera.midswap.SwapRecipes.services;
 
 
-
 import lombok.AllArgsConstructor;
 import lombok.RequiredArgsConstructor;
 import mindera.midswap.SwapRecipes.converters.RecipeConverter;
 import mindera.midswap.SwapRecipes.exceptions.RecipeNotFoundException;
+import mindera.midswap.SwapRecipes.persistence.models.Ingredient;
 import mindera.midswap.SwapRecipes.persistence.models.Recipe;
 import mindera.midswap.SwapRecipes.commands.RecipeDto;
+import mindera.midswap.SwapRecipes.persistence.repositories.IngredientJPARepository;
 import mindera.midswap.SwapRecipes.persistence.repositories.RecipeJPARepository;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @AllArgsConstructor
@@ -19,6 +21,9 @@ public class RecipeService implements RecipeServiceI {
 
     private final RecipeConverter recipeConverter;
     private final RecipeJPARepository recipeRepository;
+
+    private final IngredientJPARepository ingredientJPARepository;
+
     @Override
     public List<RecipeDto> getRecipes() {
         return this.recipeConverter.entityListToDtoList(this.recipeRepository.findAll());
@@ -31,18 +36,34 @@ public class RecipeService implements RecipeServiceI {
 
     @Override
     public RecipeDto addRecipe(RecipeDto recipeDto) {
-        Recipe saved = this.recipeRepository.save(this.recipeConverter.dtoToEntity(recipeDto));
-        return this.recipeConverter.entityToDto(saved);
+        Recipe newRecipe = new Recipe();
+        newRecipe.setId(recipeDto.getId());
+        newRecipe.setDescription(recipeDto.getDescription());
+        newRecipe.setName(recipeDto.getName());
+        newRecipe.setIngredientsIds(recipeDto.getIngredients()
+                .stream()
+                .map(ing -> {
+                            Ingredient ingredient = ing;
+                            if (this.ingredientJPARepository.findById(ingredient.getId()).isPresent()) {
+                                ingredient = this.ingredientJPARepository.findById(ingredient.getId())
+                                        .orElseThrow();
+                            }
+                    ingredient.addRecipe(this.recipeConverter.dtoToEntity(recipeDto));
+                            return ingredient;
+
+                        })
+                .collect(Collectors.toSet()));
+        return this.recipeConverter.entityToDto(this.recipeRepository.save(newRecipe));
+                }
+
+        @Override
+        public void removeRecipe (Long id){
+            this.recipeRepository.delete(getRecipeById(id));
+        }
+
+
+        @Override
+        public List<Recipe> getRecipesByIngredient (String ingredient){
+            return this.recipeRepository.findByIngredient(ingredient);
+        }
     }
-
-    @Override
-    public void removeRecipe(Long id) {
-        this.recipeRepository.delete(getRecipeById(id));
-    }
-
-
-   @Override
-    public List<Recipe> getRecipesByIngredient(String ingredient) {
-       return this.recipeRepository.findByIngredient(ingredient);
-   }
-}
