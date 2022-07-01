5e4a509db511681c41a0225c84cdcd8328236b33
alicjakropa <alakrop@gmail.com>
2022-07-01 10:41:34 +0200

    added copy of external api controller

---
+++ src/main/java/mindera/midswap/SwapRecipes/controllers/ExternalAPIController.java
@@ -0,0 +1,68 @@
+package mindera.midswap.SwapRecipes.controllers;
+
+import mindera.midswap.SwapRecipes.persistence.models.Recipe;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.Arrays;
+
+@RestController
+@RequestMapping("/api/v1")
+
+public class ExternalAPIController {
+    String uri = "http://www.omdbapi.com/";
+    String apikey = "PUT_HERE_YOUR_API_KEY";
+
+
+    @GetMapping(path = "recipe/{name}")
+    public ResponseEntity<Recipe> getRecipe(@PathVariable String name) {
+        String finalUri = uri + "?t=" + name + "&apikey=" + apikey;
+        System.out.println("finalUri = " + finalUri);
+        RestTemplate restTemplate = new RestTemplate();
+        ResponseEntity<Recipe> result = restTemplate.getForEntity(finalUri, Recipe.class);
+        return ResponseEntity.ok(result.getBody());
+    }
+
+    @GetMapping(path = "recipe/{search}")
+    public ResponseEntity<FilmList> getFilms(@PathVariable String search) {
+        String finalUri = uri + "?s=" + search + "&apikey=" + apikey;
+        System.out.println("finalUri = " + finalUri);
+        RestTemplate restTemplate = new RestTemplate();
+        ResponseEntity<FilmList> result = restTemplate.getForEntity(finalUri, FilmList.class);
+        return ResponseEntity.ok(result.getBody());
+    }
+
+    // Move these classes to a separate file! Only here for demonstration purposes.
+    public static class Film {
+        public String Title;
+        public String Year;
+        public String Poster;
+        public String Type;
+
+
+        @Override
+        public String toString() {
+            return "Film{" +
+                    "Title='" + Title + '\'' +
+                    ", Year='" + Year + '\'' +
+                    '}';
+        }
+    }
+
+
+    public static class FilmList {
+        public Film[] Search;
+
+        @Override
+        public String toString() {
+            return "FilmList{" +
+                    "Search=" + Arrays.toString(Search) +
+                    '}';
+        }
+    }
+
+}
