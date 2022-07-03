60d263624f789ca8133cd70c2d2ad8c3b8d5e45f
jon <joao.couto@mindswap.academy>
2022-07-03 15:14:59 +0100

    Jwt succesfully implemented

--- src/main/java/mindera/midswap/SwapRecipes/security/SecurityConstants.java
+++ src/main/java/mindera/midswap/SwapRecipes/security/SecurityConstants.java
@@ -2,7 +2,7 @@ package mindera.midswap.SwapRecipes.security;
 
 public class SecurityConstants {
     public static final String SECRET = "SECRET_KEY";
-    public static final long EXPIRATION_TIME = 86_400_000; // 24 horas
+    public static final long EXPIRATION_TIME = 120_000_000; // 33 horas
     public static final String TOKEN_PREFIX = "Bearer ";
     public static final String HEADER_STRING = "Authorization";
     public static final String SIGN_UP_URL = "/api/v1/user";
