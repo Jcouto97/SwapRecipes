eecb1c041176b1f23e6663056a8b38bc463fa07f
Elisa Moutinho <elisabmoutinho@gmail.com>
2022-07-04 10:23:16 +0100

    dataloader com password a encriptar

--- src/main/java/mindera/midswap/SwapRecipes/dataloader/DataLoader.java
+++ src/main/java/mindera/midswap/SwapRecipes/dataloader/DataLoader.java
@@ -14,6 +14,7 @@ import mindera.midswap.SwapRecipes.persistence.repositories.UserJPARepository;
 import org.modelmapper.ModelMapper;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
 
@@ -29,6 +30,7 @@ public class DataLoader implements ApplicationRunner {
     private RestTemplate restTemplate;
     private CategoryJPARepository categoryJPARepository;
     private ModelMapper modelMapper;
+    private PasswordEncoder passwordEncoder;
 
     //controllerEXT API VER
 
@@ -70,17 +72,17 @@ public class DataLoader implements ApplicationRunner {
                 User.builder().name("Elisa")
                         .citizenNumber(100000001L)
                         .username("elisamoutinho")
-                        .password("elisamoutinho")
+                        .password(passwordEncoder.encode("elisamoutinho"))
                         .build(),
                 User.builder().name("Ala")
                         .citizenNumber(100000002L)
                         .username("alakropa")
-                        .password("alakropa")
+                        .password(passwordEncoder.encode("alakropa"))
                         .build(),
                 User.builder().name("Joao")
                         .citizenNumber(100000003L)
                         .username("joaocouto")
-                        .password("joaocouto")
+                        .password(passwordEncoder.encode("joaocouto"))
                         .build()
         ));
         this.userJPARepository.saveAll(userList);
--- src/main/java/mindera/midswap/SwapRecipes/security/SecurityConstants.java
+++ src/main/java/mindera/midswap/SwapRecipes/security/SecurityConstants.java
@@ -2,6 +2,7 @@ package mindera.midswap.SwapRecipes.security;
 
 public class SecurityConstants {
     public static final String SECRET = "SECRET_KEY";
+    //comentar linha do tempo do token, o token nao expira
     public static final long EXPIRATION_TIME = 120_000_000; // 33 horas
     public static final String TOKEN_PREFIX = "Bearer ";
     public static final String HEADER_STRING = "Authorization";
