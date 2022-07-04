6429e428343cf86e6791a950a4dfdf06cf241387
Elisa Moutinho <elisabmoutinho@gmail.com>
2022-07-04 12:24:38 +0100

    Cache in Recipes

--- src/main/java/mindera/midswap/SwapRecipes/controllers/RecipeController.java
+++ src/main/java/mindera/midswap/SwapRecipes/controllers/RecipeController.java
@@ -6,6 +6,8 @@ import mindera.midswap.SwapRecipes.commands.RecipeDto;
 import mindera.midswap.SwapRecipes.commands.RecipeUpdateDto;
 import mindera.midswap.SwapRecipes.commands.UserDto;
 import mindera.midswap.SwapRecipes.services.RecipeServiceI;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -19,27 +21,65 @@ public class RecipeController {
     private final RecipeServiceI recipeService;
 
     @GetMapping
+    @Cacheable(value = "recipes")
     public List<RecipeDto> getRecipes() {
+        System.out.println("getting ALL RECIPES from DB");
         return this.recipeService.getRecipes();
     }
+    @GetMapping("/evict/allrecipes")
+    @CacheEvict(value = "recipes")
+    public void evictAllRecipes() {
+        System.out.println("Clearing getAllRecipes() cache");
+    }
 
 
+
+
+    @Cacheable(value = "recipes")
     @GetMapping("/byId/{id}")
     public RecipeDto getRecipeById(@PathVariable("id") Long id) {
+        System.out.println("getting RECIPE BY ID from DB");
         return this.recipeService.getRecipeDtoById(id);
     }
+    @CacheEvict(value = "recipes")
+    @GetMapping("/evict/recipebyid/{id}")
+    public void evictGetRecipeById(@PathVariable("id") Long id) {
+        System.out.println("Clearing getRecipeById() cache");
+    }
 
+
+
+    @Cacheable(value = "recipes")
     @GetMapping("/byCategoryId/{categoryId}")
     public List<RecipeDto> getRecipesByCategory(@PathVariable("categoryId") Long categoryId) {
+        System.out.println("getting RECIPE BY CATEGORY from DB");
         return this.recipeService.findByCategory(categoryId);
-        //        this.recipeService.findByCategory(categoryId);
+    }
+    @CacheEvict(value = "recipes")
+    @GetMapping("/evict/recipebycategory/{id}")
+    public void evictGetRecipesByCategory(@PathVariable("id") Long id) {
+        System.out.println("Clearing getRecipeByCategory() cache");
     }
 
+
+
+
+    @Cacheable(value = "recipes")
     @GetMapping("/byIngredientName/{ingredientName}")
     public List<RecipeDto> getRecipesByIngredientName(@PathVariable("ingredientName") String ingredientName) {
+        System.out.println("getting RECIPE BY INGREDIENT NAME from DB");
         return this.recipeService.getRecipesByIngredientName(ingredientName);
-
     }
+    @CacheEvict(value = "recipes")
+    @GetMapping("/evict/recipebyingredientname/{ingredientName}")
+    public void evictGetRecipesByIngredientName(@PathVariable("ingredientName") String ingredientName) {
+        System.out.println("Clearing getRecipesByIngredientName() cache");
+    }
+
+
+
+
+
     @PostMapping
     public RecipeDto addRecipe(@RequestBody RecipeDto recipeDto) {
         return this.recipeService.addRecipe(recipeDto);
