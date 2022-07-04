edfeca889290c1f54673f39754daae9a2dd78528
jon <joao.couto@mindswap.academy>
2022-07-04 12:07:07 +0100

    tests incorporated

--- README.md
+++ README.md
@@ -1,1 +1,32 @@
 # SwapRecipes
+
+
+**Summary:**
+
+To run program, download 18 files and compile in IDE or using javac in terminal
+
+
+**Also, 7 packages:**
+- Colors: has the colors used in the project (boards and Ascii art for ex);
+- Commands: game commands used by the players in the chat;
+- Field: draws 10x10 boards constructed using Point objects and a double array;
+- GameObjects: Such as Ship and ShipsEnums;
+- Point: a Point object that is determined by the x and y coordinate value;
+- Game: here resides most of the game logic and thread handling;
+- Utils: stores de Ascii Art used and Loading Animation.
+
+**Rules:**
+- Press enter to begin the game;
+- After checking your board decide whether to /ready or /random (to randomize board up to 3 times);
+- See if you are the attacker, in that case /attack using a letter and a number (/attack A 5 for example);
+- If you hit an adversaries boat, keep attacking, else adversary attacks;
+- Keep this going until one of the player has no remaining boats and wins;
+- Have fun :)!
+
+**Requirements:**
+To run program, download 62 files and compile in IDE or using javac in terminal
+
+## EXAMPLE OUTPUT
+
+![Screenshot 2022-06-02 163801](https://user-images.githubusercontent.com/103672168/171674503-8bb2a5f7-1402-4c07-833d-7c1a709ea338.png)
+
---
+++ src/test/java/mindera/midswap/SwapRecipes/service/UserServiceTest.java
@@ -0,0 +1,76 @@
+package mindera.midswap.SwapRecipes.service;
+
+import mindera.midswap.SwapRecipes.commands.UserDto;
+import mindera.midswap.SwapRecipes.converters.UserConverterI;
+import mindera.midswap.SwapRecipes.exceptions.UserNotFoundException;
+import mindera.midswap.SwapRecipes.persistence.models.Recipe;
+import mindera.midswap.SwapRecipes.persistence.models.User;
+import mindera.midswap.SwapRecipes.persistence.repositories.UserJPARepository;
+import mindera.midswap.SwapRecipes.services.UserServiceI;
+import mindera.midswap.SwapRecipes.services.UserServiceImpI;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.api.function.Executable;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import java.util.List;
+import java.util.Optional;
+
+import static mindera.midswap.SwapRecipes.MockedPojos.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+public class UserServiceTest {
+
+
+    private UserServiceI userServiceI;
+
+    @Mock
+    UserJPARepository userJPARepository;
+
+    @Mock
+    UserConverterI userConverterI;
+
+    @Mock
+    PasswordEncoder passwordEncoder;
+
+    @BeforeEach
+    public void setup() {
+        this.userServiceI = new UserServiceImpI(userJPARepository, userConverterI, passwordEncoder);
+    }
+
+    @Nested
+    class getUserByIdTest {
+
+
+        @Test
+        void getUsersSizeTest(){
+            when(userJPARepository.findAll()).thenReturn(List.of(USER_ENTITY_1, USER_ENTITY_2));
+
+            // assert
+            assertEquals(2, userJPARepository.findAll().size());
+        }
+
+
+        @Test
+        void testGetUserByIdNotFound() {   //PORQUE FUNCIONA?
+            // arrange
+            when(userJPARepository.findById(55L))
+                    .thenReturn(Optional.empty());
+
+            // atc
+            Executable action = () -> userServiceI.getUserById(55L);
+
+            // assert
+            assertThrows(UserNotFoundException.class, action);
+        }
+    }
+}
+
